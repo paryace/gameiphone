e7382b66f4feba3c592ad2d32fbffa8a854fee37
zhang <zhang___xiaofeng@zhang-xiaofeng.local>
2014-06-05 20:03:16 +0800

    代码整理

---
+++ GameGroup/PublicClass/Class/MessageService.h
@@ -0,0 +1,13 @@
+//
+//  MessageService.h
+//  GameGroup
+//
+//  Created by Marss on 14-6-5.
+//  Copyright (c) 2014年 Swallow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MessageService : NSObject
+
+@end
---
+++ GameGroup/PublicClass/Class/MessageService.m
@@ -0,0 +1,13 @@
+//
+//  MessageService.m
+//  GameGroup
+//
+//  Created by Marss on 14-6-5.
+//  Copyright (c) 2014年 Swallow. All rights reserved.
+//
+
+#import "MessageService.h"
+
+@implementation MessageService
+
+@end
