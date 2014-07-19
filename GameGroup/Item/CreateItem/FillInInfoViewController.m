1e2490e0bdbe0b2e221082871fdaae846ba23b40
zxflimit <zxflimit@gmail.com>
2014-07-19 17:49:15 +0800

    SI8MPLE

--- GameGroup/Item/CreateItem/FillInInfoViewController.m
+++ GameGroup/Item/CreateItem/FillInInfoViewController.m
@@ -13,9 +13,10 @@
     UITextView *contenttextView;
     NSInteger  m_maxZiShu;//发表字符数量
     UILabel *m_ziNumLabel;//提示文字
+    UILabel* placeholderL;
+    
 }
 @end
-
 @implementation FillInInfoViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -39,11 +40,18 @@
     [self.view addSubview:createBtn];
 
     contenttextView = [[UITextView alloc]initWithFrame:CGRectMake(10, startX+20, 300, 200)];
-     contenttextView.delegate = self;
+    contenttextView.delegate = self;
     [self.view addSubview:contenttextView];
     
+    placeholderL = [[UILabel alloc]initWithFrame:CGRectMake(15, startX+25, 200, 20)];
+    placeholderL.backgroundColor = [UIColor clearColor];
+    placeholderL.textColor = [UIColor grayColor];
+    placeholderL.text = @"填写组队描述……";
+    placeholderL.font = [UIFont systemFontOfSize:13.0];
+    [self.view addSubview:placeholderL];
+
     
-    m_ziNumLabel = [[UILabel alloc]initWithFrame:CGRectMake(320-10-10, 200+startX+20-20-10, 100, 20)];
+    m_ziNumLabel = [[UILabel alloc]initWithFrame:CGRectMake(320-10-10, 200+startX+20-20-20, 100, 20)];
     m_ziNumLabel.backgroundColor = [UIColor clearColor];
     m_ziNumLabel.font= [UIFont systemFontOfSize:12];
     m_ziNumLabel.textAlignment = NSTextAlignmentRight;
@@ -64,6 +72,11 @@
 #pragma mark - text view delegate
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
 {
+    if (textView.text.length>0 || text.length != 0) {
+        placeholderL.text = @"";
+    }else{
+        placeholderL.text = @"填写组队描述……";
+    }
     NSString *new = [textView.text stringByReplacingCharactersInRange:range withString:text];
     NSInteger res = m_maxZiShu-[[GameCommon shareGameCommon] unicodeLengthOfString:new];
     if(res >= 0){
@@ -83,7 +96,7 @@
     }
     m_ziNumLabel.text =[NSString stringWithFormat:@"%d%@%d",ziNum,@"/",m_maxZiShu];
     CGSize nameSize = [m_ziNumLabel.text sizeWithFont:[UIFont boldSystemFontOfSize:14] constrainedToSize:CGSizeMake(100, 20) lineBreakMode:NSLineBreakByWordWrapping];
-    m_ziNumLabel.frame=CGRectMake(320-5-nameSize.width, startX+148,nameSize.width,20);
+    m_ziNumLabel.frame=CGRectMake(320-nameSize.width-10-10, 200+startX+20-20-10, nameSize.width, 20);
     m_ziNumLabel.backgroundColor=[UIColor clearColor];
 }
 #pragma mark - touch
