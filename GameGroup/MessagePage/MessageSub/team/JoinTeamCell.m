ea2e8e06d3e8a45b49271bd783977829ceb965c4
Apple <zxflimit@gmail.com>
2014-07-24 15:55:18 +0800

    no message

--- GameGroup/MessagePage/MessageSub/team/JoinTeamCell.m
+++ GameGroup/MessagePage/MessageSub/team/JoinTeamCell.m
@@ -22,44 +22,46 @@
         [self.bgV addSubview:self.headImageV];
     
         
-        self.groupNameLable = [[UILabel alloc]initWithFrame:CGRectMake(55, 12, 100, 20)];
+        self.groupNameLable = [[UILabel alloc]initWithFrame:CGRectMake(75, 12, 100, 20)];
         self.groupNameLable.backgroundColor = [UIColor clearColor];
         self.groupNameLable.textColor = kColorWithRGB(5,5,5, 0.7);
         self.groupNameLable.text = @"群名";
         self.groupNameLable.font =[ UIFont systemFontOfSize:12];
         [self.bgV addSubview:self.groupNameLable];
         
-        self.genderImageV = [[UIImageView alloc]initWithFrame:CGRectMake(160, 10, 20, 20)];
+        self.genderImageV = [[UIImageView alloc]initWithFrame:CGRectMake(180, 10, 20, 20)];
         [self.bgV addSubview:self.genderImageV];
         
-        self.positionLable = [[UILabel alloc]initWithFrame:CGRectMake(185, 10, 50, 20)];
+        self.positionLable = [[UILabel alloc]initWithFrame:CGRectMake(205, 13, 30, 15)];
         self.positionLable.backgroundColor = [UIColor blueColor];
         self.positionLable.textColor = kColorWithRGB(5,5,5, 0.7);
         self.positionLable.text = @"职位";
+        self.positionLable.textColor = [UIColor whiteColor];
         self.positionLable.layer.masksToBounds = YES;
         self.positionLable.layer.cornerRadius = 3.0;
         self.positionLable.font =[ UIFont systemFontOfSize:12];
         [self.bgV addSubview:self.positionLable];
         
-        self.gameImageV = [[EGOImageView alloc]initWithFrame:CGRectMake(55, 35, 25, 25)];
+        self.gameImageV = [[EGOImageView alloc]initWithFrame:CGRectMake(75, 35, 25, 25)];
         [self.bgV addSubview:self.gameImageV];
         
-        self.realmLable = [[UILabel alloc]initWithFrame:CGRectMake(85, 37, 200, 20)];
+        self.realmLable = [[UILabel alloc]initWithFrame:CGRectMake(105, 37, 200, 20)];
         self.realmLable.backgroundColor = [UIColor clearColor];
         self.realmLable.textColor = kColorWithRGB(5,5,5, 0.7);
         self.realmLable.text = @"服务器名";
         self.realmLable.font =[ UIFont systemFontOfSize:12];
         [self.bgV addSubview:self.realmLable];
         
-        self.pveLable = [[UILabel alloc]initWithFrame:CGRectMake(55, 62, 300, 20)];
+        self.pveLable = [[UILabel alloc]initWithFrame:CGRectMake(75, 62, 300, 20)];
         self.pveLable.backgroundColor = [UIColor clearColor];
         self.pveLable.textColor = kColorWithRGB(5,5,5, 0.7);
         self.pveLable.text = @"战斗力";
         self.pveLable.font =[ UIFont systemFontOfSize:12];
         [self.bgV addSubview:self.pveLable];
         
-        self.timeLable = [[UILabel alloc]initWithFrame:CGRectMake(200, 12, 100, 20)];
+        self.timeLable = [[UILabel alloc]initWithFrame:CGRectMake(220, 12, 80, 20)];
         self.timeLable.backgroundColor = [UIColor clearColor];
+        self.timeLable.textAlignment = NSTextAlignmentRight;
         self.timeLable.textColor = kColorWithRGB(5,5,5, 0.7);
         self.timeLable.text = @"时间";
         self.timeLable.font =[ UIFont systemFontOfSize:12];
