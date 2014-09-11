008fc0c8bf909066a6051810a38ed16ef0dbfb97
weixing0311 <345301696@qq.com>
2014-09-11 10:54:15 +0800

    更改联系人界面

---
+++ GameGroup/FriendPage/FriendFirstCell.h
@@ -0,0 +1,14 @@
+//
+//  FriendFirstCell.h
+//  GameGroup
+//
+//  Created by 魏星 on 14-9-11.
+//  Copyright (c) 2014年 Swallow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendFirstCell : UITableViewCell
+@property (nonatomic,strong)UIImageView * headImageView;
+@property (nonatomic,strong)UILabel *titleLabel;
+@end
