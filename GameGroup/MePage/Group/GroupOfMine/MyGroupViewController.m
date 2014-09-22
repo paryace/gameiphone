cd236aec99743b9ff2370af247d07f84c5294551
Apple <zxflimit@gmail.com>
2014-09-22 19:06:25 +0800

    simple

--- GameGroup/MePage/Group/GroupOfMine/MyGroupViewController.m
+++ GameGroup/MePage/Group/GroupOfMine/MyGroupViewController.m
@@ -62,18 +62,13 @@ static NSString * const HeaderIdentifier = @"HeaderIdentifier";
     [self setTopViewWithTitle:@"我的组织" withBackButton:YES];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshNet:) name:@"refelsh_groupInfo_wx" object:nil];
-
-    
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(refreshNet:) name:@"RefreshMyGroupList" object:nil];
-    
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(receivedBillboardMsg:) name:Billboard_msg object:nil];
-
     myGroupArray = [NSMutableArray array];
     
     UIButton *shareButton = [[UIButton alloc]initWithFrame:CGRectMake(320-65, KISHighVersion_7?20:0, 65, 44)];
     [shareButton setBackgroundImage:KUIImage(@"createGroup_normal") forState:UIControlStateNormal];
     [shareButton setBackgroundImage:KUIImage(@"createGroup_click") forState:UIControlStateHighlighted];
-//    [shareButton setTitle:@"创建" forState:UIControlStateNormal];
     shareButton.titleLabel.font = [UIFont systemFontOfSize: 18.0];
     [shareButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [shareButton setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
@@ -91,45 +86,9 @@ static NSString * const HeaderIdentifier = @"HeaderIdentifier";
     m_myTableView.dataSource = self;
     m_myTableView.delegate = self;
     m_myTableView.backgroundColor = UIColorFromRGBA(0xf3f3f3, 1);
-//    if(KISHighVersion_7){
-//        m_myTableView.sectionIndexBackgroundColor = [UIColor clearColor];
-//    }
-//    m_myTableView.sectionIndexTrackingBackgroundColor = [UIColor clearColor];
-//    m_myTableView.sectionIndexColor = UIColorFromRGBA(0xbcbcbc, 1);
-    
     [self.view addSubview:m_myTableView];
 
     [GameCommon setExtraCellLineHidden:m_myTableView];
-    
-    
-//    imageHight = (320-15)/4;
-//    m_layout = [[UICollectionViewFlowLayout alloc]init];
-//    m_layout.minimumInteritemSpacing = 1;
-//    m_layout.minimumLineSpacing =5;
-//    m_layout.itemSize = CGSizeMake(imageHight, imageHight);
-//    m_layout.headerReferenceSize = CGSizeMake(320, 70);
-//    m_layout.sectionInset = UIEdgeInsetsMake(10,5,3,3);
-//    
-//    groupCollectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, startX, 320, 70+10+imageHight+5) collectionViewLayout:m_layout];
-//    groupCollectionView.backgroundColor = UIColorFromRGBA(0xf8f8f8, 1);
-//    groupCollectionView.scrollEnabled = YES;
-//    groupCollectionView.delegate = self;
-//    groupCollectionView.dataSource = self;
-//    [groupCollectionView registerClass:[GroupOfMineCell class] forCellWithReuseIdentifier:@"titleCell"];
-//    [groupCollectionView registerClass:[ReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:HeaderIdentifier];
-//    groupCollectionView.backgroundColor = [UIColor clearColor];
-//    [self.view addSubview:groupCollectionView];
-    
-    
-//    cellView = [[UIView alloc]initWithFrame:CGRectMake(0, startX+(70+10+imageHight+5), 320, 200)];
-//    UILabel *lajiLabel= [[ UILabel alloc]initWithFrame:CGRectMake(0, 10, 320, 20)];
-//    lajiLabel.backgroundColor = [UIColor clearColor];
-//    lajiLabel.textColor = [UIColor grayColor];
-//    lajiLabel.text = @"立即添加游戏组织,开始更好的游戏体验!";
-//    lajiLabel.textAlignment = NSTextAlignmentCenter;
-//    lajiLabel.font = [UIFont systemFontOfSize:12];
-//    [cellView addSubview:lajiLabel];
-//    [self.view addSubview:cellView];
     [self loadCacheGroupList];
     [self getGroupListFromNet];
 }
@@ -210,13 +169,16 @@ static NSString * const HeaderIdentifier = @"HeaderIdentifier";
 -(void)setGroupList:(NSMutableArray*)responseObject
 {
     [myGroupArray removeAllObjects];
-//    myGroupArray = [responseObject mutableCopy];
-   NSArray *arr = [responseObject sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-         return (NSComparisonResult)NSOrderedAscending;
+    NSArray *array222 = [responseObject sortedArrayUsingComparator:^NSComparisonResult(NSMutableDictionary * obj1, NSMutableDictionary * obj2) {
+        if ([KISDictionaryHaveKey(obj1, @"groupId") integerValue] > [KISDictionaryHaveKey(obj2, @"groupId") integerValue]) {
+            return (NSComparisonResult)NSOrderedDescending;
+        }
+        if ([KISDictionaryHaveKey(obj1, @"groupId") integerValue] < [KISDictionaryHaveKey(obj2, @"groupId") integerValue]) {
+            return (NSComparisonResult)NSOrderedAscending;
+        }
+        return (NSComparisonResult)NSOrderedSame;
     }];
-    myGroupArray = [NSMutableArray arrayWithArray:arr];
-//    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:@"addphoto",@"backgroundImg", nil];
-//    [myGroupArray addObject:dic];
+    myGroupArray = [NSMutableArray arrayWithArray:array222];
     if (myGroupArray.count<4) {
         groupCollectionView.frame = CGRectMake(0, startX, 320, 70+10+imageHight+5);
         cellView.frame = CGRectMake(0, startX+(70+10+imageHight+5), 320, 200);
@@ -248,11 +210,6 @@ static NSString * const HeaderIdentifier = @"HeaderIdentifier";
     NSString * gameId = [GameCommon getNewStringWithId:KISDictionaryHaveKey(cellDic, @"gameid")];
     NSString * imageId = [GameCommon putoutgameIconWithGameId:gameId];
     cell.gameImageView.imageURL = [ImageService getImageUrl4:imageId];
-//    NSURL *url = [ImageService getImageStr2: imageId];
-//    EGOImageView * EGOimage = [[EGOImageView alloc]init];
-//    EGOimage.imageURL = url;
-//    cell.gameImageView.imageURL = [ImageService getImageStr:imageId Width:100];
-//    cell.gameImageView.image = EGOimage.image;
     cell.memberCountLable.text = [NSString stringWithFormat:@"%@/%@",KISDictionaryHaveKey(cellDic, @"currentMemberNum"),KISDictionaryHaveKey(cellDic, @"maxMemberNum")];
     cell.describeLable.text = [GameCommon getNewStringWithId:KISDictionaryHaveKey(cellDic, @"info")];
     cell.leveLable.text = [GameCommon getNewStringWithId:KISDictionaryHaveKey(cellDic, @"level")];
@@ -275,59 +232,6 @@ static NSString * const HeaderIdentifier = @"HeaderIdentifier";
     [self.navigationController pushViewController:gr animated:YES];
 
 }
-
-
-//-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-//{
-//    NSLog(@"-------%d",myGroupArray.count);
-//    return myGroupArray.count;
-//}
-//
-//-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    GroupOfMineCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"titleCell" forIndexPath:indexPath];
-//    NSMutableDictionary * cellDic = [myGroupArray objectAtIndex:indexPath.row];
-//    if (indexPath.row ==myGroupArray.count-1) {
-//        NSString *imgStr = [NSString stringWithFormat:@"%@",KISDictionaryHaveKey(cellDic, @"backgroundImg")];
-//        cell.headImgView.placeholderImage = KUIImage(imgStr);
-//        cell.headImgView.imageURL = nil;
-//        cell.titleLabel.backgroundColor = [UIColor clearColor];
-//        cell.titleLabel.text = @"";
-//    }else{
-//        cell.headImgView.placeholderImage = KUIImage(@"group_icon");
-//        cell.headImgView.imageURL = [ImageService getImageUrl3:KISDictionaryHaveKey(cellDic, @"backgroundImg") Width:120];
-//        cell.titleLabel.backgroundColor  =[UIColor colorWithRed:0/255.0f green:0/255.0f blue:0/255.0f alpha:0.5];
-//        cell.titleLabel.text = KISDictionaryHaveKey(cellDic, @"groupName");
-//    }
-//    return cell;
-//}
-//-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    NSDictionary *dic = [myGroupArray objectAtIndex:indexPath.row];
-////    if (indexPath.row ==myGroupArray.count-1) {
-////        NewSearchGroupController *joinIn = [[NewSearchGroupController alloc]init];
-////        joinIn.gameid = self.gameid;
-////        [self.navigationController pushViewController:joinIn animated:YES];
-////    }else{
-//        GroupInformationViewController *gr = [[GroupInformationViewController alloc]init];
-//        gr.groupId =KISDictionaryHaveKey(dic, @"groupId");
-//        gr.isAudit = NO;
-//        [self.navigationController pushViewController:gr animated:YES];
-////    }
-//}
-//
-//-(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
-//{
-//    UICollectionReusableView *titleView;
-//    if (kind == UICollectionElementKindSectionHeader) {
-//        titleView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:HeaderIdentifier forIndexPath:indexPath];
-//        ReusableView * reu = ((ReusableView *)titleView);
-//        reu.delegate = self;
-//        [reu setInfo:dict setMsgCount:msgCount];
-//    }
-//    return titleView;
-//}
-
 //点击
 -(void)onClick:(UIButton*)sender
 {
